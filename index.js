eeee456f3fac0b19075ff2047ce64bb48d133cbc
Sardor <tojimuradovsardor25@gmail.com>
2021-09-23 12:37:10 +0500

    takrorlash
    hbs va pug

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+//pug ulash
+app.set('views engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+app.set('views engine', 'pug')
+
+app.get('/', (req, res) => {
+    res.render('index.pug',{
+        title:'home page'
+    })
+})
+app.get('/contact', (req, res) => {
+    res.render('contact.pug',{
+        title:'contact page'
+    })
+})
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Express working on 3000 port`);
+})
