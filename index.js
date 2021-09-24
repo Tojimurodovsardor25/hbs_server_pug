8024ec6ed5fbff05cd51fe662e0df4711d00070e
Sardor <tojimuradovsardor25@gmail.com>
2021-09-24 08:09:46 +0300

    davom etish kerak
    HBS davomi

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const express = require("express")
 const app = express()
-const path = require('path')
+// const path = require('path')
+const exphbs = require('express-hadlebars')
 
-app.use(express.static(path.join(__dirname, 'public')))
+const hbs = exphbs.create()
+
+app.engine('hbs',exphbs())
+
+// app.use(express.static(path.join(__dirname, 'public')))
 
 
 //pug ulash
-app.set('views engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
+// app.set('views engine', 'pug')
+// app.set('views', path.join(__dirname, 'views'))
 
-app.set('views engine', 'pug')
+// app.set('views engine', 'pug')
 
-app.get('/', (req, res) => {
-    res.render('index.pug',{
-        title:'home page'
-    })
-})
-app.get('/contact', (req, res) => {
-    res.render('contact.pug',{
-        title:'contact page'
-    })
-})
+// app.get('/', (req, res) => {
+//     res.render('index.pug',{
+//         title:'home page'
+//     })
+// })
+// app.get('/contact', (req, res) => {
+//     res.render('contact.pug',{
+//         title:'contact page'
+//     })
+// })
 
 const port = 3000
 
